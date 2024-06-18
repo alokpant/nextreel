008ceff91217caefd45cfd1f4661467784667ed6
mindl3ss <justtodesign@gmail.com>
2024-06-18 10:53:13 +0200

    fix: update to make Logo a href which takes back to root page

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
-  return <span className="bg-amber-400 text-amber-900 py-1 px-2 text-xl rounded font-bold">IMDB</span>
+  return <Link href="/" className="bg-amber-400 text-amber-900 py-1 px-2 text-xl rounded font-bold">IMDB</Link>
 }
