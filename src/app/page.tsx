09b9386beee4b1a8e4a2ab268257bce657677a2e
mindl3ss <justtodesign@gmail.com>
2024-06-27 11:03:58 +0200

    fix: cleanup data for trending

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { ApiParamType } from "@/components/subHeader/SubHeaderItem";
 import topTrending from '../data/topTrending.json';
 import topRated from '../data/topRated.json';
+import { useSearchParams } from 'next/navigation'
+import { ApiSuccessResponse } from "./types";
 
 const ApiParam = {
   TOP_TRENDING: 'trending',
@@ -12,27 +14,24 @@ export const API_GENRE_URL = {
   [ApiParam.TOP_RATED]: '/trending/all/week',
 } as const;
 
+
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }:  Record<string, any>) {
   const genre: ApiParamType = searchParams.genre || ApiParam.TOP_TRENDING;
-  const apiUrl = `${API_URL}${API_GENRE_URL[genre]}?api_key=${API_KEY}x&language=en-US&page=1`;
+  const apiUrl = `${API_URL}${API_GENRE_URL[genre]}?api_key=${API_KEY}&language=en-US&page=1`;
 
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    console.log('=========================');
-    console.log('API URL:', apiUrl);
-    console.log('Response Data:', data);
+    const data: ApiSuccessResponse = await response.json();
 
     if (!response.ok) {
       throw new Error('Failed to see the result');
     }
 
-    const result = data.results;
-    const display = result.map(({ id, original_title }) => <li key={id}>{original_title}</li>);
+    const results = data.results;
+    const display = results.map(({ id, original_title }) => <li key={id}>{original_title}</li>);
 
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ src/app/types.ts
@@ -0,0 +1,43 @@
+interface TopRated {
+  adult: boolean,
+  backdrop_path: string,
+  genre_ids: string[],
+  id: number,
+  original_language: string,
+  original_title: string,
+  overview: string,
+  popularity: number,
+  poster_path: string,
+  release_date: string,
+  title: string,
+  video: boolean,
+  vote_average: number,
+  vote_count: number
+}
+
+interface TopTrending {
+  backdrop_path: string,
+  id: number,
+  title: string,
+  original_title: string,
+  overview: string,
+  poster_path: string,
+  media_type: string,
+  adult: boolean,
+  name: string,
+  original_language: string,
+  genre_ids: number[],
+  popularity: number,
+  first_air_date: string,
+  release_date: string,
+  vote_average: number,
+  video: false,
+  vote_count: number,
+}
+
+export interface ApiSuccessResponse {
+  page: number
+  results: (TopRated | TopTrending)[]
+  total_pages: number
+  total_result: number
+}
