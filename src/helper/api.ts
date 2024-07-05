3a273b2e8b42d4603453795f251162d20ccb2209
mindl3ss <justtodesign@gmail.com>
2024-07-05 13:13:25 +0200

    fix: update url builder

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -2,8 +2,13 @@ const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL || '';
 
 export default function buildApiUrl(apiPath: string) {
-	// return `${API_URL}${apiPath}?api_key=${API_KEY}&language=en-US&page=1`;
-	return API_URL.concat(`${apiPath}?api_key=${API_KEY}&language=en-US&page=1`);
+  console.log(`API_URL: ${API_URL}`);
+  console.log(`apiPath: ${apiPath}`);
+  
+  const baseUrl = API_URL.endsWith('/') ? API_URL.slice(0, -1) : API_URL;
+  const path = apiPath.startsWith('/') ? apiPath : `/${apiPath}`;
+  
+  return `${baseUrl}${path}?api_key=${API_KEY}&language=en-US&page=1`;
 }
 
 export function buildSearchApiUrl(searchTerm: string) {
