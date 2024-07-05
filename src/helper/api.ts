a69c32baaca185fac1d3d12512c9b04566518cd8
mindl3ss <justtodesign@gmail.com>
2024-07-05 13:00:45 +0200

    fix: update apiurlbuilder to use concat instead of template literals

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -1,8 +1,9 @@
 const API_KEY = process.env.API_KEY;
-const API_URL = process.env.API_URL;
+const API_URL = process.env.API_URL || '';
 
 export default function buildApiUrl(apiPath: string) {
-	return `${API_URL}${apiPath}?api_key=${API_KEY}&language=en-US&page=1`;
+	// return `${API_URL}${apiPath}?api_key=${API_KEY}&language=en-US&page=1`;
+	return API_URL.concat(`${apiPath}?api_key=${API_KEY}&language=en-US&page=1`);
 }
 
 export function buildSearchApiUrl(searchTerm: string) {
